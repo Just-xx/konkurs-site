9fee68270a55ead8e18c4c3db0d7b1cbc2f506c2
Just-xx <kubadreczz@gmail.com>
2023-10-22 23:43:40 +0200

    byg fix number quatro

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './Header.css'
 import Logo from '../Logo/Logo'
 import Button from '../Button/Button'
-import { Link } from 'react-router-dom'
 import { handleCSVFile } from '../../utils/handleCSVFile'
 import { useContext } from 'react'
 import { CSVDataContext } from '../../context/CSVDataContext';
@@ -16,7 +15,7 @@ export default function Header() {
     <header>
       <Logo />
       <nav>
-        <Link className='navlink' to="prezentacja" target='__blank' rel="noopener noreferrer"><span>Przejdź do wyników</span><i className="fa-solid fa-up-right-from-square"></i></Link>
+        <a className='navlink' to="prezentacja" target='__blank'><span>Przejdź do wyników</span><i className="fa-solid fa-up-right-from-square"></i></a>
         <button onClick={() => fileDownload(CSVData)} className="navlink">Eksportuj wyniki</button>
         <Button fileInput onChange={e => handleCSVFile(e, setCSVData)}>Załaduj z pliku</Button>
       </nav>
