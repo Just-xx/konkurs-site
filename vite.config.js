04b6d6d6f8be6de87f8350f203b4bf31530a9928
Just-xx <kubadreczz@gmail.com>
2023-10-22 23:28:42 +0200

    bug fix number uno

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     watch: {
-      usePolling: true
-    }
-  }
-})
+      usePolling: true,
+    },
+  },
+  external: ["react", "react-router", "react-router-dom"]
+});
