8e5ad5a4a8f559c4f1ff3df9565eabc618f833f8
Just-xx <kubadreczz@gmail.com>
2023-10-23 14:17:32 +0200

    link bug fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,7 @@ import { handleCSVFile } from '../../utils/handleCSVFile'
 import { useContext } from 'react'
 import { CSVDataContext } from '../../context/CSVDataContext';
 import { fileDownload } from '../../utils/downloadFile'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
 
@@ -15,7 +16,7 @@ export default function Header() {
     <header>
       <Logo />
       <nav>
-        <a className='navlink' to="prezentacja" target='__blank'><span>Przejdź do wyników</span><i className="fa-solid fa-up-right-from-square"></i></a>
+        <Link className='navlink' to="prezentacja" target="_blank" rel="noopener noreferrer" ><span>Przejdź do wyników</span><i className="fa-solid fa-up-right-from-square"></i></Link>
         <button onClick={() => fileDownload(CSVData)} className="navlink">Eksportuj wyniki</button>
         <Button fileInput onChange={e => handleCSVFile(e, setCSVData)}>Załaduj z pliku</Button>
       </nav>
