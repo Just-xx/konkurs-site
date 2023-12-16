b2b5a3e25a4a3a477526b8d28e0d82e7972eaabe
Just-xx <kubadreczz@gmail.com>
2023-12-16 22:05:44 +0100

    link position

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,12 +10,12 @@ export default function Header() {
 
   return (
     <header>
-      <Logo />
-      <nav>
         <Link className='navlink' to="prezentacja" target="_blank" rel="noopener noreferrer" ><span>Przejdź do wyników</span><i className="fa-solid fa-up-right-from-square"></i></Link>
+      {/* <Logo /> */}
+      {/* <nav> */}
         {/* <button onClick={() => fileDownload(CSVData)} className="navlink">Eksportuj wyniki</button> */}
         {/* <Button fileInput onChange={e => handleCSVFile(e, setCSVData)}>Załaduj z pliku</Button> */}
-      </nav>
+      {/* </nav> */}
     </header>
   )
 }
